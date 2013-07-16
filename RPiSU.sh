2f83e0806012e8b5ab77723e26f1c8c89ab65e5f
schmron <schmron@gmail.com>
2013-07-16 19:35:17 +0200

    Export exist check added
    If script executed, you get an error (line 3). Now the script check if gpio already exported

--- RPiSU.sh
+++ RPiSU.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-echo "25" > /sys/class/gpio/unexport
-echo "25" > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio25/direction
-echo "1" > /sys/class/gpio/gpio25/value
+GPIO=25
+
+if [ ! -f "/sys/class/gpio/gpio$GPIO" ]
+then
+	echo "$GPIO" > /sys/class/gpio/export
+fi
+echo "out" > "/sys/class/gpio/gpio$GPIO/direction"
+echo "1" > "/sys/class/gpio/gpio$GPIO/value"
 
 sleep 1
 
-echo "in" > /sys/class/gpio/gpio25/direction
+echo "in" > "/sys/class/gpio/gpio$GPIO/direction"
 while [ true ]
 do
-	if [ "$(cat /sys/class/gpio/gpio25/value)" == '1' ]; then
-		echo "out" > /sys/class/gpio/gpio25/direction
-		echo "1" > /sys/class/gpio/gpio25/value
+	if [ "$(cat /sys/class/gpio/gpio$GPIO/value)" == '1' ]; then
+		echo "out" > "/sys/class/gpio/gpio$GPIO/direction"
+		echo "1" > "/sys/class/gpio/gpio$GPIO/value"
 		shutdown -h now
 		exit 1
 	fi
